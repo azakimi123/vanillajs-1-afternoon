e6838df84ab587d5e15cdcae0fe943851f8dbea3
azakimi123 <aaron.zakimi@gmail.com>
2020-06-10 14:45:18 -0600

    step 3

--- Part1/index.js
+++ Part1/index.js
@@ -1,4 +1,22 @@
-// Test JS connection //
+// Step 1 //
 // const name = "Aaron";
 // console.log(name);
 
+
+
+function play(clickedId) {
+    console.log(clickedId)
+    const clickedElement = document.getElementById(clickedId);
+    const playerSpan = document.getElementById('player');
+    if (playerSpan.innerText === 'X') {
+        playerSpan.innerText = 'O';
+        clickedElement.innerText = 'X';
+    }   else {
+        playerSpan.innerText = 'X';
+        clickedElement.innerText = 'O';
+    }
+}
+
+
+// console.log(document.getElementById('test'));
+// console.log(player.innerText);
