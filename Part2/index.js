53c7e86b8bc8139faf1fc482cc692ecbcf9baa11
azakimi123 <aaron.zakimi@gmail.com>
2020-06-10 22:29:25 -0600

    4 of a kind

--- Part2/index.js
+++ Part2/index.js
@@ -1,1 +1,11 @@
-console.log('Aaron')
+
+const idInput = document.getElementById('idInput');
+const colorInput = document.getElementById('colorInput');
+
+// console.log(idInput)
+// console.log(colorInput)
+
+function setCard() {
+    const card = document.getElementById(idInput.value);
+    card.style.color = colorInput.value;
+}
